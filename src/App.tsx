758147cd9830fb118b6ee7d497a842d32cb1fed2
Your Name <santiagoyanez1990@gmail.com>
2025-03-02 21:15:00 +0100

    initial components basic structure with imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { useState } from "react";
-import { Calendar } from "Calendar.jsx";
+import React, { useState, useEffect } from "react";
+import {
+  addDays,
+  addMonths,
+  startOfWeek,
+  startOfMonth,
+  endOfWeek,
+  endOfMonth,
+  isSameMonth,
+  isSameDay,
+  format,
+} from "date-fns";
+import Calendar from "Calendar.tsx";
+import Event from "Event.tsx";
+import EventModal from "EventModal.tsx";
+import OverFlowModal from "OverFlowModal.tsx";
 import "./App.css";
+import "index.css";
 
 export default function App() {
   return (
     <>
+      const [value, setValue] = useState();
+      <DatePicker value={value} onChange={setValue} />
       <p>Initial Component</p>
     </>
   );
--- src/EventModal.tsx
+++ src/EventModal.tsx
@@ -1,4 +1,3 @@
-import react from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export function EventModal() {}
